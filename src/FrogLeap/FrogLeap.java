365214dbe16c3a159f538076117879fddc2a42fb
MihailTs <mihail.tsvetkov.04@gmail.com>
2025-10-06 00:21:24 +0300

    small changes

--- src/FrogLeap/FrogLeap.java
+++ src/FrogLeap/FrogLeap.java
@@ -7,7 +7,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class FrogLeap {
 
-    static StringBuilder state = new StringBuilder(" ");
+    static StringBuilder state = new StringBuilder("_");
 
 //    public static void dfs(StringBuilder state, String finalState, int spacePosition, int size,
 //                           ArrayList<String> statesPath, AtomicBoolean solutionFound) {
@@ -22,33 +22,33 @@ public class FrogLeap {
 //
 //        if(spacePosition - 1 >= 0 && state.charAt(spacePosition - 1) == '>') {
 //            state.setCharAt(spacePosition, '>');
-//            state.setCharAt(spacePosition - 1, ' ');
+//            state.setCharAt(spacePosition - 1, '_');
 //            dfs(state, finalState, spacePosition - 1, size, statesPath, solutionFound);
-//            state.setCharAt(spacePosition, ' ');
+//            state.setCharAt(spacePosition, '_');
 //            state.setCharAt(spacePosition - 1, '>');
 //        }
 //
 //        if(spacePosition - 2 >= 0 && state.charAt(spacePosition - 2) == '>') {
 //            state.setCharAt(spacePosition, '>');
-//            state.setCharAt(spacePosition - 2, ' ');
+//            state.setCharAt(spacePosition - 2, '_');
 //            dfs(state, finalState, spacePosition - 2, size, statesPath, solutionFound);
-//            state.setCharAt(spacePosition, ' ');
+//            state.setCharAt(spacePosition, '_');
 //            state.setCharAt(spacePosition - 2, '>');
 //        }
 //
 //        if(spacePosition + 1 < size && state.charAt(spacePosition + 1) == '<') {
 //            state.setCharAt(spacePosition, '<');
-//            state.setCharAt(spacePosition + 1, ' ');
+//            state.setCharAt(spacePosition + 1, '_');
 //            dfs(state, finalState, spacePosition + 1, size, statesPath, solutionFound);
-//            state.setCharAt(spacePosition, ' ');
+//            state.setCharAt(spacePosition, '_');
 //            state.setCharAt(spacePosition + 1, '<');
 //        }
 //
 //        if(spacePosition + 2 < size && state.charAt(spacePosition + 2) == '<') {
 //            state.setCharAt(spacePosition, '<');
-//            state.setCharAt(spacePosition + 2, ' ');
+//            state.setCharAt(spacePosition + 2, '_');
 //            dfs(state, finalState, spacePosition + 2, size, statesPath, solutionFound);
-//            state.setCharAt(spacePosition, ' ');
+//            state.setCharAt(spacePosition, '_');
 //            state.setCharAt(spacePosition + 2, '<');
 //        }
 //
@@ -57,44 +57,42 @@ public class FrogLeap {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n;
+        int n = sc.nextInt();
 
-        do {
-            n = sc.nextInt();
-        } while(n % 2 == 0);
-
-        StringBuilder finalState = new StringBuilder(" ");
-        for(int i = 0; i < n / 2; i++) {
+//        StringBuilder finalState = new StringBuilder("_");
+        for(int i = 0; i < n; i++) {
             state.insert(0, ">");
             state.append("<");
-            finalState.insert(0, "<");
-            finalState.append(">");
+//            finalState.insert(0, "<");
+//            finalState.append(">");
         }
 
         int stepCnt = 1;
         boolean leftFrTurn = true;
-        int spacePosition = n / 2;
+        int spacePosition = n;
         int helpCnt = 3;
+
+        System.out.println(state);
         while(stepCnt > 0) {
             for(int i = 0; i < stepCnt; i++) {
                 if(leftFrTurn) {
                     if(state.charAt(spacePosition - 1) == '>') {
                         state.setCharAt(spacePosition, '>');
-                        state.setCharAt(spacePosition - 1, ' ');
+                        state.setCharAt(spacePosition - 1, '_');
                         spacePosition--;
                     } else if(state.charAt(spacePosition - 2) == '>') {
                         state.setCharAt(spacePosition, '>');
-                        state.setCharAt(spacePosition - 2, ' ');
+                        state.setCharAt(spacePosition - 2, '_');
                         spacePosition -= 2;
                     }
                 } else {
                     if(state.charAt(spacePosition + 1) == '<') {
                         state.setCharAt(spacePosition, '<');
-                        state.setCharAt(spacePosition + 1, ' ');
+                        state.setCharAt(spacePosition + 1, '_');
                         spacePosition++;
                     } else if(state.charAt(spacePosition + 2) == '<') {
                         state.setCharAt(spacePosition, '<');
-                        state.setCharAt(spacePosition + 2, ' ');
+                        state.setCharAt(spacePosition + 2, '_');
                         spacePosition += 2;
                     }
                 }
@@ -102,14 +100,14 @@ public class FrogLeap {
             }
 
             leftFrTurn = !leftFrTurn;
-            if(stepCnt == n / 2) helpCnt--;
+            if(stepCnt == n) helpCnt--;
             if(helpCnt == 3) stepCnt++;
             if(helpCnt == 0) stepCnt--;
         }
 
-//        int spacePosition = n / 2;
+//        int spacePosition = n;
 //        ArrayList<String> statesPath = new ArrayList<>();
 //        AtomicBoolean solutionFound = new AtomicBoolean(false);
-//        dfs(state, finalState.toString(), spacePosition, n, statesPath, solutionFound);
+//        dfs(state, finalState.toString(), spacePosition, 2*n + 1, statesPath, solutionFound);
     }
 }
